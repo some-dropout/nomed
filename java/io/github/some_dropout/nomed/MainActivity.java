56ae8cac747ea4e46115e1e3a2d9f56b4568f494
dropout <223595116+some-dropout@users.noreply.github.com>
2025-07-31 09:15:15 +0900

    drop example code
    Signed-off-by: dropout <223595116+some-dropout@users.noreply.github.com>

---
+++ java/io/github/some_dropout/nomed/MainActivity.java
@@ -0,0 +1,12 @@
+package io.github.some_dropout.nomed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+  @Override
+  public void onCreate(Bundle bundle)
+  {
+    super.onCreate(bundle);
+  }
+}
--- jni/lib.c
+++
@@ -1,8 +0,0 @@
-#include "com_example_app_MainActivity.h"
-
-JNIEXPORT jint JNICALL
-Java_com_example_app_MainActivity_getNum (JNIEnv *env, jobject thiz)
-{
-  // do stuffs here
-  return 3 + 4;
-}
